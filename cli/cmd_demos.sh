0b5f564227e421a1d33a61b500fd799122be2195
cpucorecore <cpucorecore@gmail.com>
2021-03-25 16:29:32 +0800

    add Everifier Deploy and verify cmd

--- cli/cmd_demos.sh
+++ cli/cmd_demos.sh
@@ -11,3 +11,14 @@ node index.js ELC deploy -b 35249 -u http://localhost:8545 http://localhost:7545
 
 # ELC relay
 node index.js ethRelay relay http://localhost:8545 http://localhost:7545 0xcE5E56cBA007bCf2137C878618461E73d342A113
+
+# get Proof of a tx on eth
+# node index.js EProve proof ethRPC txHash
+node index.js EProve proof http://localhost:8545 0xc008be9258da8b4c1da836caa5adbded5837cea7532dd52a2de37b860b0d22a3
+
+# deploy EVerifier contract
+node index.js EVerifier deploy http://localhost:7545
+#0x8AbfDd996D7334bF01c25F482Cac3B7b6310Ebe0
+
+# use EVerifier to verify contract
+node index.js EVerifier verify http://localhost:8545 0xc008be9258da8b4c1da836caa5adbded5837cea7532dd52a2de37b860b0d22a3 http://localhost:7545 0x8AbfDd996D7334bF01c25F482Cac3B7b6310Ebe0
